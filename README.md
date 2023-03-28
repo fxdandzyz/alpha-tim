eb53abab98cfe659fb93b443f633a3b0abb108ec
fxdandzyz <fxd18629436147@gmail.com>
2023-03-28 19:19:17 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,88 @@
+# Realistic evaluation of transductive few-shot learning
+
+
+##  Introduction
+This repo contains the code for our NeurIPS 2021 paper "Realistic evaluation of transductive few-shot learning". This is a framework that regroups all methods evaluated in our paper except for SIB and LR-ICI. Results provided in the paper can be reproduced with this repo. Code was developed under python 3.8.3 and pytorch 1.4.0.
+
+
+## 1. Getting started
+
+
+### 1.1 Quick installation (recommended) (Download datasets and models)
+To download datasets and pre-trained models (checkpoints), follow instructions 1.1.1 to 1.1.2 of NeurIPS 2020 paper "TIM: Transductive Information Maximization" public implementation (https://github.com/mboudiaf/TIM)
+
+#### 1.1.1 Place datasets
+Make sure to place the downloaded datasets (data/ folder) at the root of the directory.
+
+#### 1.1.2 Place models
+Make sure to place the downloaded pre-trained models (checkpoints/ folder) at the root of the directory.
+
+### 1.2 Manual installation
+Follow instruction 1.2 of NeurIPS 2020 paper "TIM: Transductive Information Maximization" public implementation (https://github.com/mboudiaf/TIM) if facing issues with previous steps. Make sure to place data/ and checkpoints/ folders at the root of the directory.
+
+### 2. Requirements
+To install requirements:
+```bash
+conda create --name <env> --file requirements.txt
+```
+Where \<env> is the name of your environment
+
+## 3. Reproducing the main results
+
+Before anything, activate the environment:
+```python
+source activate <env>
+```
+
+### 3.1 Table 1 and 2 results in paper
+
+### Evaluation in a 5-shot scenario on ***mini***-Imagenet using RN-18 as backbone (Table 1. in paper)  
+| Method        | 1-shot |  5-shot |  10-shot |  20-shot |
+| ---           |  ---   | ---     | ---      | ---      | 
+| SimpleShot    |  63.0  | 80.1    | 84.0     | 86.1     | 
+| PT-MAP        |  60.1 (&#8595;16.8) | 67.1 (&#8595;18.2)| 68.8 (&#8595;18.0)| 70.4 (&#8595;17.4)|
+| LaplacianShot | 65.4 (&#8595;4.7)  | 81.6 (&#8595;0.5)| 84.1 (&#8595;0.2)| 86.0 (&#8593;0.5)|
+| BDCSPN        | 67.0 (&#8595;2.4)  | 80.2 (&#8595;1.8)| 82.7 (&#8595;1.4)| 84.6 (&#8595;1.1)|   
+| TIM           |  67.3 (&#8595;4.5)  |  79.8 (&#8595;4.1)|  82.3 (&#8595;3.8)|  84.2 (&#8595;3.7)|
+| &alpha;-TIM  | **67.4**  | **82.5** | **85.9** | **87.9** | 
+
+To reproduce the results from Table 1. and 2. in the paper, from the root of the directory execute this python command.
+```python
+python3 -m src.main --base_config <path_to_base_config_file> --method_config <path_to_method_config_file> 
+```
+
+The <path_to_base_config_file> follows this hierarchy:
+```python
+config/<balanced or dirichlet>/base_config/<resnet18 or wideres>/<mini or tiered or cub>/base_config.yaml
+```
+
+The <path_to_method_config_file> follows this hierarchy:
+```python
+config/<balanced or dirichlet>/methods_config/<alpha_tim or baseline or baseline_pp or bdcspn or entropy_min or laplacianshot or protonet or pt_map or simpleshot or tim>.yaml
+```
+
+For instance, if you want to reproduce the results in the **balanced** setting on mini-Imagenet, using ResNet-18, with alpha-TIM method go to the root of the directory and execute:
+```python
+python3 -m src.main --base_config config/balanced/base_config/resnet18/mini/base_config.yaml --method_config config/balanced/methods_config/alpha_tim.yaml
+```
+
+If you want to reproduce the results in the **randomly balanced** setting on mini-Imagenet, using ResNet-18, with alpha-TIM method go to the root of the directory and execute:
+```python
+python3 -m src.main --base_config config/dirichlet/base_config/resnet18/mini/base_config.yaml --method_config config/dirichlet/methods_config/alpha_tim.yaml
+```
+
+# Reusable data sampler module
+One of our main contribution is our realistic task sampling method following Dirichlet's distribution. 
+![plot](plots/dirichlet_density_plot.png)
+
+Our realistic sampler can be found in [sampler.py](src/datasets/sampler.py) file. The sampler has been implemented following Pytorch's norms and in a way that it can be easily reused and integrated in other projects.
+
+The following notebook [exemple_realistic_sampler.ipynb](exemple_realistic_sampler.ipynb) is an exemple that shows how to initialize and use our realistic category sampler.
+
+# Contact
+For further questions or details, reach out to Olivier Veilleux (olivier.veilleux.2@ens.etsmtl.ca)
+
+# Acknowledgements
+Special thanks to the authors of NeurIPS 2020 paper "TIM: Transductive Information Maximization" (TIM) (https://github.com/mboudiaf/TIM) for publicly sharing their pre-trained models and their source code from which this repo was inspired from.
+
+
---
+++ src/train_mstar.py
@@ -0,0 +1,225 @@
+import torch
+import time
+import random
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+import parser
+import torch.backends.cudnn as cudnn
+from functools import reduce
+from visdom_logger import VisdomLogger
+from src.utils import warp_tqdm, save_checkpoint, load_cfg_from_cfg_file, merge_cfg_from_list, Logger, get_log_file
+from src.utils import warp_tqdm, get_metric, AverageMeter,save_checkpoint
+from src.utils_mstar import get_training_dataloader,get_val_dataloader,get_test_dataloader,compute_mean_std
+from src.mstar import MstarTrain,MstarTest
+from src.models.ingredient import get_model
+from src.optim import get_optimizer, get_scheduler
+
+class Trainer:
+    def __init__(self,device,args):
+        self.device=device
+        self.args=args
+        self.train_dataset=MstarTrain(args.dataset_path,transform=args.transform)
+        self.train_mean,self.train_std=compute_mean_std(self.train_dataset)
+        self.train_loader=get_training_dataloader(mean=self.train_mean,std=self.train_std,batch_size=args.batch_size,
+                                                  num_workers=args.num_workers,shuffle=True)
+        self.val_dataset=MstarTrain(args.dataset_path,transform=args.transform)
+        self.val_mean,self.val_std=compute_mean_std(self.val_dataset)
+        self.val_loader=get_val_dataloader(mean=self.val_mean,std=self.val_std,batch_size=args.batch_size,
+                                                  num_workers=args.num_workers,shuffle=True)
+        self.num_classes=args.num_classes
+    def ce_loss(self,log_pred, targets, use_hard_labels=True, reduction='none'):
+        """
+        wrapper for cross entropy loss in pytorch.
+    
+        Args
+        logits: logit values, shape=[Batch size, # of classes]
+        targets: integer or vector, shape=[Batch size] or [Batch size, # of classes]
+        use_hard_labels: If True, targets have [Batch size] shape with int values. If False, the target is vector (default True)
+        """
+        if use_hard_labels:
+            return F.nll_loss(log_pred, targets, reduction=reduction)
+            # return F.cross_entropy(logits, targets, reduction=reduction) this is unstable
+        else:
+            assert log_pred.shape == targets.shape
+            nll_loss = torch.sum(-targets * log_pred, dim=1)
+            return nll_loss
+    def do_epoch(self, epoch, scheduler, print_freq, disable_tqdm, callback, model,
+                 alpha, optimizer):
+        batch_time = AverageMeter()
+        losses = AverageMeter()
+        top1 = AverageMeter()
+
+        # switch to train mode
+        model.train()
+        steps_per_epoch = len(self.train_loader)
+        end = time.time()
+        tqdm_train_loader = warp_tqdm(self.train_loader, disable_tqdm)
+        for i, (input, target, _) in enumerate(tqdm_train_loader):
+
+            input, target = input.to(self.device), target.to(self.device, non_blocking=True)
+
+            smoothed_targets = self.smooth_one_hot(target)
+            assert (smoothed_targets.argmax(1) == target).float().mean() == 1.0
+            # Forward pass
+            if alpha > 0:  # Mixup augmentation
+                # generate mixed sample and targets
+                lam = np.random.beta(alpha, alpha)
+                rand_index = torch.randperm(input.size()[0]).cuda()
+                target_a = smoothed_targets
+                target_b = smoothed_targets[rand_index]
+                mixed_input = lam * input + (1 - lam) * input[rand_index]
+
+                output = model(mixed_input)
+                loss = self.cross_entropy(output, target_a) * lam + self.cross_entropy(output, target_b) * (1. - lam)
+            else:
+                output = model(input)
+                log_pred=F.log_softmax(output,dim=-1)
+                loss = self.ce_loss(log_pred, smoothed_targets)
+
+            # Backward pass
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            prec1 = (output.argmax(1) == target).float().mean()
+            top1.update(prec1.item(), input.size(0))
+            if not disable_tqdm:
+                tqdm_train_loader.set_description('Acc {:.2f}'.format(top1.avg))
+
+            # Measure accuracy and record loss
+            losses.update(loss.item(), input.size(0))
+            batch_time.update(time.time() - end)
+            end = time.time()
+
+            if i % print_freq == 0:
+                print('Epoch: [{0}][{1}/{2}]\t'
+                      'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                      'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+                      'Prec@1 {top1.val:.3f} ({top1.avg:.3f})\t'.format(
+                       epoch, i, len(self.train_loader), batch_time=batch_time,
+                       loss=losses, top1=top1))
+                if callback is not None:
+                    callback.scalar('train_loss', i / steps_per_epoch + epoch, losses.avg, title='Train loss')
+                    callback.scalar('@1', i / steps_per_epoch + epoch, top1.avg, title='Train Accuracy')
+        scheduler.step()
+        for param_group in optimizer.param_groups:
+            current_lr = param_group['lr']
+        if callback is not None:
+            callback.scalar('lr', epoch, current_lr, title='Learning rate')
+
+    def smooth_one_hot(self, targets):
+        assert 0 <= self.args.label_smoothing < 1
+        with torch.no_grad():
+            new_targets = torch.empty(size=(targets.size(0), self.num_classes), device=self.device)
+            new_targets.fill_(self.args.label_smoothing / (self.num_classes-1))
+            new_targets.scatter_(1, targets.unsqueeze(1), 1. - self.args.label_smoothing)
+        return new_targets
+
+    def meta_val(self, model, disable_tqdm, callback, epoch):
+        top1 = AverageMeter()
+        model.eval()
+
+        with torch.no_grad():
+            tqdm_test_loader = warp_tqdm(self.val_loader, disable_tqdm)
+            for i, (inputs, target, _) in enumerate(tqdm_test_loader):
+                inputs, target = inputs.to(self.device), target.to(self.device, non_blocking=True)
+                output = model(inputs, feature=True)[0].cuda(0)
+                train_out = output[:self.args.meta_val_way * self.args.meta_val_shot]
+                train_label = target[:self.args.meta_val_way * self.args.meta_val_shot]
+                test_out = output[self.args.meta_val_way * self.args.meta_val_shot:]
+                test_label = target[self.args.meta_val_way * self.args.meta_val_shot:]
+                train_out = train_out.reshape(self.args.meta_val_way, self.args.meta_val_shot, -1).mean(1)
+                train_label = train_label[::self.args.meta_val_shot]
+                prediction = self.metric_prediction(train_out, test_out, train_label)
+                acc = (prediction == test_label).float().mean()
+                top1.update(acc.item())
+                if not disable_tqdm:
+                    tqdm_test_loader.set_description('Acc {:.2f}'.format(top1.avg * 100))
+
+        if callback is not None:
+            callback.scalar('val_acc', epoch + 1, top1.avg, title='Val acc')
+        return top1.avg
+
+    def metric_prediction(self, support, query, train_label):
+        support = support.view(support.shape[0], -1)
+        query = query.view(query.shape[0], -1)
+        distance = get_metric(self.args.meta_val_metric)(support, query)
+        predict = torch.argmin(distance, dim=1)
+        predict = torch.take(train_label, predict)
+        return predict
+    def getValAcc(self,model):
+        acc=0
+        batch_acc=[]
+        for i,(inputs,targets,_) in enumerate(self.val_loader):
+            inputs, target = inputs.to(self.device), target.to(self.device, non_blocking=True)
+            output = model(inputs, feature=True)[0].cuda(0)
+            output = torch.max(torch.softmax(output,dim=-1),dim=-1)[1]
+            batch_acc.append(torch.sum(target.reshape(-1)==output.reshape(-1)).div(self.args.batch_size))
+        acc=reduce(lambda x,y:x+y,batch_acc)
+        return acc
+def main():
+    args=parser.args()
+    device = torch.device("cuda" if args.cuda else "cpu")
+    callback = None if args.visdom_port is None else VisdomLogger(port=args.visdom_port)
+    if args.seed is not None:
+        random.seed(args.seed)
+        torch.manual_seed(args.seed)
+        cudnn.deterministic = True
+    torch.cuda.set_device(0)
+    # init logger
+    log_file = get_log_file(log_path=args.log_path, dataset=args.dataset,
+                            backbone=args.arch, method=args.method)
+    logger = Logger(__name__, log_file)
+
+    # create model
+    logger.info("=> Creating model '{}'".format(args.arch))
+    model = torch.nn.DataParallel(get_model(args)).cuda()
+    logger.info('Number of model parameters: {}'.format(sum([p.data.nelement() for p in model.parameters()])))
+    optimizer = get_optimizer(args=args, model=model)
+    
+    start_epoch = 0
+    best_prec1 = -1
+
+    cudnn.benchmark = True
+     # If this line is reached, then training the model
+    trainer = Trainer(device=device, args=args)
+    scheduler = get_scheduler(optimizer=optimizer,
+                              num_batches=len(trainer.train_loader),
+                              epochs=args.epochs,
+                              args=args)
+    tqdm_loop = warp_tqdm(list(range(start_epoch, args.epochs)),
+                          disable_tqdm=False)
+    for epoch in tqdm_loop:
+        # Do one epoch
+        trainer.do_epoch(model=model, optimizer=optimizer, epoch=args.epochs,
+                         scheduler=scheduler, disable_tqdm=False,
+                         callback=callback,print_freq=args.print_freq,alpha=args.alpha)
+        #此处有改动，我们不再采用原方法的场景式训练方法，而是采用基于迁移学习的小样本学习方法
+        prec1 = trainer.getValAcc(model)
+        logger.info('Meta Val {}: {}'.format(epoch, prec1))
+        is_best = prec1 > best_prec1
+        best_prec1 = max(prec1, best_prec1)
+
+        tqdm_loop.set_description('Best Acc {:.2f}'.format(best_prec1 * 100.))
+
+        # Save checkpoint
+        save_checkpoint(state={'epoch': epoch + 1,
+                               'arch': args.arch,
+                               'state_dict': model.state_dict(),
+                               'best_prec1': best_prec1,
+                               'optimizer': optimizer.state_dict()},
+                        is_best=is_best,
+                        folder=args.ckpt_path)
+        if scheduler is not None:
+            scheduler.step()
+
+    # Final evaluation on test set
+    logger.info("=> Creating model '{}'".format(args.arch))
+    model = torch.nn.DataParallel(get_model(args)).cuda()
+
+    logger.info('Number of model parameters: {}'.format(sum([p.data.nelement() for p in model.parameters()])))
+    
+if __name__ == "__main__":
+    import pdb
+    pdb.set_trace()
+    main()
